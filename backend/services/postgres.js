2773fe67ef302fedfc3d2e71fd70639e52bdb76a
Anirudh <anirudhterdal@gmail.com>
2023-02-14 11:59:18 +0000

    Created table with name and score

--- backend/services/postgres.js
+++ backend/services/postgres.js
@@ -7,4 +7,18 @@ const sql = postgres({
     password             : 'g1raffe',
 });
 
+await sql`
+
+CREATE TABLE all_plays (
+    column1 name,
+    column2 score,
+    column3 time,
+    column4 location,
+)
+
+SELECT column1, column3, column4
+FROM all_plays
+ORDER BY column2 DESC
+`;
+
 export default sql;
--- backend/src/controller/index.js
+++ backend/src/controller/index.js
@@ -1,7 +1,9 @@
 import sql from '../../services/postgres.js';
 
 export const someAction = async (res, parameters) => {
-    await sql`CREATE TABLE IF NOT EXISTS example (a INTEGER, b TEXT)`;
+    await sql`
+    CREATE TABLE IF NOT EXISTS example (a INTEGER, b TEXT)
+    `;
     await sql`INSERT INTO example (a, b) VALUES (1, 'hello')`;
     const data = await sql`SELECT * FROM example`;
     
