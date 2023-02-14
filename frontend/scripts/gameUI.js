0dd40c8702dafc451328c01d8d5a0c0b4defbc66
Zaineb <zainebalmahdi110@gmail.com>
2023-02-14 12:09:40 +0000

    first draft trying to make a grid

---
+++ frontend/scripts/gameUI.js
@@ -0,0 +1,24 @@
+// Box width
+var bw = 35;
+// Box height
+var bh = 35;
+// Padding
+var p = 10;
+
+var canvas = document.getElementById("Grid");
+var context = canvas.getContext("2d");
+function drawBoard(){
+    for (var x = 0; x <= bw; x += 40) {
+        context.moveTo(0.5 + x + p, p);
+        context.lineTo(0.5 + x + p, bh + p);
+    }
+
+    for (var x = 0; x <= bh; x += 40) {
+        context.moveTo(p, 0.5 + x + p);
+        context.lineTo(bw + p, 0.5 + x + p);
+    }
+    context.strokeStyle = "black";
+    context.stroke();
+}
+
+drawBoard();
