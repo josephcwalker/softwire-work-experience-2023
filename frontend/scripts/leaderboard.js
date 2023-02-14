478f2c30cd5915f7f8e42864bb940a12469d32a3
Angela <angela.aijia.zhang@gmail.com>
2023-02-14 12:01:36 +0000

    We added a header, a button and a leadership table

---
+++ frontend/scripts/leaderboard.js
@@ -0,0 +1,29 @@
+function load(){
+    var userscores = {
+      "ex1": 10,
+      "noncy": 40,
+      "del3tus": 24,
+      "the_r0ck": 8,
+      "MONSTER_OSITY": 120
+    };
+    var max = 0;
+    var sorted = [];
+    for(var prop in userscores){
+      if(userscores[prop] >= max){
+        max = userscores[prop];
+      }
+    }
+    var cur = max;
+    for(var i = max; i > 0; i--){
+      for(var prop in userscores){
+        if(userscores[prop] == i){
+          sorted.push(prop);
+        }
+      }
+    }
+    var html = "";
+    for(var i = 0; i < sorted.length; i++){
+      html = "<tr><td>" + (i + 1) + "</td><td>" + sorted[i] + "</td><td>" + userscores[sorted[i]] + "</td></tr>";
+      document.getElementById("leaderboard").innerHTML += html;
+    }
+  }
