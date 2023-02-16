22d1688db2f49c43541f2dc68944de9c6859a245
Zaineb <zainebalmahdi110@gmail.com>
2023-02-16 12:12:35 +0000

    managed to display random starting piece in random colour on playfield and link to java. Moved our functions to our js file and called gameLogicInterface subroutines. problem: playfield is upside down.

--- frontend/scripts/gameUI.js
+++ frontend/scripts/gameUI.js
@@ -1,24 +1,123 @@
-// Box width
-var bw = 350;
-// Box height
-var bh = 700;
-// Padding
-var p = 10;
+import createGame, { Tetromino } from "./gameLogicInterface.js"
 
-var canvas = document.getElementById("Grid");
-var context = canvas.getContext("2d");
-function drawBoard(){
-    alert("running?")
-    for (var x = 0; x <= bw; x += 40) {
-        context.moveTo(0.5 + x + p, p);
-        context.lineTo(0.5 + x + p, bh + p);
-    }
+var cnv=document.getElementById("Grid");
+var ctx = cnv.getContext("2d");
+for(var i=0; i<=700; i=i+35){
+        ctx.moveTo(0, i);
+        ctx.lineTo(700,i);
+        
+        ctx.strokeStyle="#264653";
+        ctx.stroke();  
+}
+for(var i=0; i<=700; i=i+35){
+        ctx.moveTo(i,0);
+        ctx.lineTo(i,700);
 
-    for (var x = 0; x <= bh; x += 40) {
-        context.moveTo(p, 0.5 + x + p);
-        context.lineTo(bw + p, 0.5 + x + p);
-    }
-    context.strokeStyle = "black";
-    context.stroke();
+        
+
+        ctx.strokeStyle="#264653";
+        ctx.stroke();
+
 }
 
+
+const options = [
+        "cyan",     
+        "blue",     
+        "orange",     
+        "yellow", 
+        "green",
+        "purple",
+        "red"
+        ];
+
+
+
+
+let game = createGame()
+
+
+
+function drawsquare(x, y, color){
+    var canvas= document.getElementById("Grid");
+    var ctx= canvas.getContext("2d");
+
+    ctx.fillStyle= color ;
+    ctx.fillRect(x, y, 35, 35);
+}
+
+// generates random block colors
+
+function generateFill(){
+
+    for(i = 0; i < 699;i+=35 ){
+        
+    
+        for(j = 0; j < 349;j+=35 ){
+
+        
+            
+            let x = (Math.random());
+            
+
+
+
+
+            if (x<0.5){
+                continue
+            
+
+            }else {
+                let y = Math.floor(Math.random()*7)-1;
+                drawsquare(j,i,options[y])
+
+            }
+
+        }
+
+    }
+}
+
+
+function drawTiles(){
+        var i;
+        var j;
+        for(i = 0; i < 699;i+=35 ){
+                for(j = 0; j < 349;j+=35 ){
+                        
+                        let temp_i = i/35;
+                        let temp_j = j/35;
+                        let temp = game.getTileAtPosition(temp_j, temp_i);
+                        if (temp == null){
+                                continue;
+                        }else {
+                                let color = temp;
+
+
+                                drawsquare(j,i,temp);
+    
+                        }
+    
+                }
+    
+        }
+}
+drawTiles()
+
+
+
+// e
+
+
+function drawTetromino(x, y, piece, color){
+    alert("yippe")
+    for (b=0; b<3;b++){
+            for(z=0; z<3;z++){
+                if (piece[b][z] = 0 ){
+                        continue
+                } else{
+                        drawsquare(x, y, color)
+                }
+            }
+    }
+}
